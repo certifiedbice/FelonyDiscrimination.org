ac250ba2ecab1d7c7e1ae949b007ea12ff7281d9
Bice <59291964+certifiedbice@users.noreply.github.com>
2020-08-12 17:00:21 -0600

    Added logic for NODE_ENV API_ENPOINT

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
-export default {
-	API_ENDPOINT:'https://limitless-reef-44232.herokuapp.com/api',
-	//API_ENDPOINT:'http://localhost:8000/api',
-	API_KEY:process.env.REACT_APP_API_KEY,
+module.exports={
+	NODE_ENV:process.env.NODE_ENV||'development',
+	API_ENDPOINT:(process.env.NODE_ENV==='development')
+		? 'http://localhost:8000/api'
+		: 'https://limitless-reef-44232.herokuapp.com/api'
 }
